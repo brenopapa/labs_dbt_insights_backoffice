28a6e2ab25deb7affe388ae4005de931c04ae12c
Breno Papa <bpapa@outlook.com.br>
2023-09-29 17:36:38 -0300

    using exposures on dms

--- models/datamodels/accounting_forecast.sql
+++
@@ -1,4 +0,0 @@
-with lineage as (
-    select 1 from {{ source('insights_backoffice', 'matching_suggestions') }} where false
-)
-select * from lineage
--- models/datamodels/financial_forecast.sql
+++
@@ -1,4 +0,0 @@
-with test as (
-    select * from {{ source('insights_backoffice', 'se1') }}
-)
-select * from test
